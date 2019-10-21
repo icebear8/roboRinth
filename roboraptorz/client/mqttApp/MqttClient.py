a7cffe199fd72a0c7ace37fa981ad71da5dce997
Niklas Hofmann <niklas.hofmann@graph-tech.ch>
2019-10-21 05:57:11 -0700

    Initial commit of roboraptorz client

---
+++ roboraptorz/client/mqttApp/MqttClient.py
@@ -0,0 +1,79 @@
+
+import logging
+import random
+import sys
+
+import paho.mqtt.client as mqtt
+
+import Handlers as handler
+
+logger = logging.getLogger(__name__)
+
+_defaultHost="localhost"
+_defaultPort=1883
+
+mqttSubscriptions = [
+  "robo-01/notification/#",
+]
+
+mqttSubscriptionHandlers = {
+  "robo-01/notification/color/#":   handler.handleColor,
+  "robo-01/notification/gyro/#":    handler.handleGyro,
+}
+
+class MqttClient:
+  def __init__(self, host=_defaultHost, port=_defaultPort, clientId=None):
+    if clientId is None:
+      clientId = "mqttapp_" + str(random.randint(1, sys.maxsize))
+
+    logger.debug("Init client, host: " + str(host) + "; port: " + str(port) + "; clientId: " + clientId)
+
+    self._host = host
+    self._port = port
+    self._keepalive = 60
+
+    self._client = mqtt.Client(client_id=clientId, clean_session=True)
+    self._client.on_connect = self._onConnect
+    self._client.on_subscribe = self._onSubscribe
+    self._client.on_unsubscribe = self._onUnsubscribe
+    self._client.on_disconnect = self._onDisconnect
+    self._client.on_message = self._onMessage
+
+  def startAsync(self):
+    self._client.connect(self._host, self._port, self._keepalive)
+    self._client.loop_start();
+
+  def stop(self):
+    self._client.loop_stop();
+    self._client.disconnect()
+
+  def _subscribeTopics(self, topics):
+    for topic in topics:
+      self._client.subscribe(topic)
+
+  def _setupMessageHandler(self, handlers):
+    for handler in handlers:
+      self._client.message_callback_add(handler, handlers[handler])
+
+  def _setupNotifications(self):
+    client.publish("robo-01/subscribe/color/name")
+    client.publish("robo-01/subscribe/gyro/angle")
+
+  def _onConnect(self, client, userdata, flags, rc):
+    logger.debug("Connected with result code " + str(rc))
+    self._subscribeTopics(mqttSubscriptions)
+    self._setupMessageHandler(mqttSubscriptionHandlers)
+    self._setupNotifications()
+
+  def _onDisconnect(self, client, userdata, rc):
+    if rc != 0:
+      logger.warn("Unexpected disconnection")
+
+  def _onSubscribe(self, client, userdata, mid, granted_qos):
+    logger.debug("Subscribed " + str(mid))
+
+  def _onUnsubscribe(self, client, userdata, mid, granted_qos):
+    logger.debug("Unsubscribed " + str(mid))
+
+  def _onMessage(self, client, userdata, msg):
+    logger.debug("Unhandled message: " + msg.topic + " " + str(msg.payload))
---
+++ roboraptorz/client/mqttApp/mqttapp.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+import getopt
+import logging
+import sys
+import time
+
+from MqttClient import MqttClient
+
+logger = logging.getLogger(__name__)
+
+client = None
+
+def _initializeLogging(loglevel):
+  numeric_level = getattr(logging, loglevel.upper(), None)
+  if not isinstance(numeric_level, int):
+    numeric_level = getattr(logging, INFO, None)
+
+  logging.basicConfig(format='%(asctime)s %(levelname)s:%(name)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=numeric_level)
+  logging.Formatter.converter = time.gmtime
+
+def _printUsage():
+  print('mqttapp.py [--host=, --port=, --log=]')
+  print('--host=: Host name or IP to connect')
+  print('--port=: Port to connect')
+  print('--clientId=: MQTT client connection id')
+  print('--log=: Loglevel [DEBUG, INFO, WARNING, ERROR, CRITICAL]')
+
+def main(argv):
+
+  host = "localhost"
+  port = 1883
+  loglevel = "DEBUG"
+  clientId = None
+
+  # Readout arguments
+  try:
+    opts, args = getopt.getopt(argv, "h", ["help", "host=", "port=", "clientId=", "log="])
+  except getopt.GetoptError as err:
+    print(err)  # will print something like "option -a not recognized"
+    _printUsage()
+    sys.exit(2)
+
+  for opt, arg in opts:
+    if opt in ('-h', '--help'):
+      _printUsage()
+      sys.exit()
+    elif opt in ('--host'):
+      host = arg
+    elif opt in ('--port'):
+      try:
+        port = int(arg)
+      except ValueError:
+        pass
+    elif opt in ('--log='):
+      loglevel = arg
+
+  _initializeLogging(loglevel)
+  logger.debug("Main started")
+
+  # Setup mqtt client
+  client = MqttClient(host, port, clientId)
+  client.startAsync()
+  time.sleep(2)
+
+  input("\n\nPress Enter to abort...\n\n")
+
+  # Terminate
+  client.stop()
+
+  logger.debug("Terminate")
+
+if __name__ == '__main__':
+  main(sys.argv[1:])
