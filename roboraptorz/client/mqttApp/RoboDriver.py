2923ed3e8cd94c43513046c68600800609e46037
ponder.bear <ponder.bear@protonmail.com>
2019-10-21 15:21:22 +0200

    Pseudo robo driver documentation

---
+++ roboraptorz/client/mqttApp/RoboDriver.py
@@ -0,0 +1,13 @@
+
+
+
+def driveDirection(direction=North)
+def discoverMode(enabled=true)
+
+
+# callback 'onStatus', enum idle, busy
+def getStatus()
+
+# callback
+def onDirections(list of directions, as enum)
+def onColors(list of directions as enum, and colors as string)
