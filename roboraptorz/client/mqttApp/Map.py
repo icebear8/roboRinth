5ccb19b09589c534917d8e4c02c62bfa707ed551
Niklas Hofmann <niklas.hofmann@graph-tech.ch>
2019-10-21 17:28:42 +0200

    Initial draft for map and mapmapper

--- roboraptorz/client/mqttApp/Map.py
+++ roboraptorz/client/mqttApp/Map.py
@@ -1,3 +1,47 @@
 
+import logging
+
+logger = logging.getLogger(__name__)
+
+class Point:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class RelativeDirection:
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    def __init__(self, dx, dy):
+        self.dx = dx
+        self.dy = dy
+
 class Map:
-    pass
+    ROBOT_LOCATION_KEY = "robotLocation"
+    START_POINT_KEY = "startPoint"
+    SYNC_LINE_KEY = "syncLine"
+
+    def __init__(self):
+        self.mapPoints = []
+        self.pointsOfInteres = {}
+
+    def addMapPoint(self,  direction):
+        logger.debug("")
+        self.mapPoints.append(direction)
+
+    def setRobotLocation(self, point):
+        self.pointsOfInteres[self.ROBOT_LOCATION_KEY] = point
+
+    def getRobotLocation(self):
+        return self.pointsOfInteres[self.ROBOT_LOCATION_KEY]
+
+    def setStartPoint(self, point):
+        self.pointsOfInteres[self.START_POINT_KEY] = point
+
+    def setSyncLine(self, direction):
+        self.pointsOfInteres[self.SYNC_LINE_KEY] = direction
--- roboraptorz/client/mqttApp/MapMatcher.py
+++ roboraptorz/client/mqttApp/MapMatcher.py
@@ -7,5 +7,33 @@ logger = logging.getLogger(__name__)
 
 class MapMatcher:
     def __init__(self):
-        self.globalMap = Map();
+        self.globalMap = Map()
         self.roboMaps = {}
+
+    def registerRobotDriver(self, robotId, robotDriver):
+        self.roboMaps[robotId] = Map()
+        callback = lambda dirs : self.onDirectionsCallback(robotId, dirs)
+        robotDriver.onDirections = callback
+
+    def onDirectionsCallback(self, robotId, availableDirections):
+        map = self.roboMaps[robotId]
+        currentPosition = map.getRobotLocation()
+        for direction in availableDirections:
+            dx, dy = 0
+            if direction == RoboDirection.NORTH:
+                dx = 0
+                dy = 1
+            elif direction == RoboDirection.EAST:
+                dx = 1
+                dy = 0
+            elif direction == RoboDirection.SOUTH:
+                dx = 0
+                dy = -1
+            else:
+                dx = -1
+                dy = 0
+            relativeDirection = RelativeDirection(dx, dy)
+            map.addMapPoint((currentPosition, relativeDirection))
+
+
+
