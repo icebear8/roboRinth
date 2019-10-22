bf918fce96cc9fd5a742fce3368cc4109b544fc8
ponder.bear <ponder.bear@protonmail.com>
2019-10-22 12:09:40 +0200

    Extend robo controller

--- roboraptorz/client/mqttApp/Control.py
+++ roboraptorz/client/mqttApp/Control.py
@@ -1,5 +1,6 @@
 
 import logging
+import threading
 
 from Map import *
 from MapMatcher import *
@@ -13,7 +14,6 @@ class Control:
 
     self._roboDriver=roboDriver
     self._roboDriver.onStatus=self._onRoboStatus
-    self._roboDriver.onColors=self._onRoboColors
 
     self._mapMatcher=mapMatcher
     self._mapMatcher.onMapUpdate=self._onMapUpdate
@@ -29,18 +29,43 @@ class Control:
   def _onMapUpdate(mapName):
     if mapName in (self.roboName):
       logger.debug(self.roboName + "OnMapUpdate: " + mapName)
+      self._triggerExploreStep()
 
   # Callbacks driver
   def _onRoboStatus(self, status):
     logger.debug(self.roboName + "OnRoboStatus: " + str(status))
+    if status is RoboStatus.IDLE:
+      self._triggerExploreStep()
 
-  def _onRoboColors(self, colors):
-    logger.debug(self.roboName + "OnRoboColors: " + str(colors))
 
-  def _exploreStep(self):
+  def _triggerExploreStep(self):
+    timer = threading.Timer(1, self._executeExploreStep())
+    timer.start()
+
+  def _executeExploreStep(self):
     if self._roboDriver.getStatus() is RoboStatus.IDLE:
-      nextStep =
-      self._roboDriver.driveDirection()
+      nextDirection = _calcNextExploreStep()
+      logger.debug(self.roboName + "exploreStep, nextStep to be executed: " + str(nextDirection))
+      if nextDirection:
+        self._roboDriver.driveDirection(nextDirection)
 
   def _calcNextExploreStep():
-    pass
+    directions = self._map().getAvailableDirections()
+    logger.debug(self.roboName + "calcNextExploreStep, available directions: " + str(directions))
+
+    # find unexplored neighbours
+    unexplored = filter(lambda element: element.explored==False, directions)
+    if unexplored:
+      logger.debug(self.roboName + "calcNextExploreStep, unexplored list: " + str(unexplored))
+      sortedDir = unexplored.sort(key=lambda element: element.direction)
+      return sortedDir[0].direction
+
+    # find closest unexplored child
+    unexploredChild = filter(lambda element: element.hasUnexploredChildren==True, directions)
+    if unexploredChild:
+      logger.debug(self.roboName + "calcNextExploreStep, unexploredChildren list: " + str(unexploredChild))
+      sortedDir = unexploredChild.sort(key=lambda element: element.distanceToUnexplored)
+      return sortedDir[0].direction
+
+    logger.info(self.roboName + "calcNextExploreStep, No unexplored neighbours or children!")
+    return None
