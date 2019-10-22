e6bbe8486c2b56dea3944ac676c9dfcb4bb1d191
dme <kogi@gmx.ch>
2019-10-22 10:27:31 +0200

    FollowLine: stop on crossing based on distance

--- robocontrolz/mqttApp/FollowLine.py
+++ robocontrolz/mqttApp/FollowLine.py
@@ -11,7 +11,7 @@ class FollowLineState(enum.Enum):
 
 class FollowLineMotorSpeed(enum.Enum):
     stop = 1
-    slow = 2
+    crossing = 2
     fast = 3
 
 class followLine:
@@ -30,13 +30,10 @@ class followLine:
                 logger.debug("got Black while driving")
             if (msg.startswith("W")):
                 logger.info("reach crossing")
-                self.motorSpeed(FollowLineMotorSpeed.slow)
+                self.motorSpeed(FollowLineMotorSpeed.crossing)
         if (self.currentState == FollowLineState.reachCrossing):
             if (msg.startswith("W")):
                 logger.debug("got Whilte while in crossing")
-            if (msg.startswith("B")):
-                self.motorSpeed(FollowLineMotorSpeed.stop)
-                logger.debug("got Black while in crossing, stop")
 
     def handleStartDriving(self, msg):
         self.motorSpeed(FollowLineMotorSpeed.fast)
@@ -46,9 +43,9 @@ class followLine:
             self.currentState = FollowLineState.stopped
             self._client.publish(self._roboName+"/request/steering/activate", "{\"speed\":\"0\",\"steering\":\"0\"}")
             return
-        elif (speed == FollowLineMotorSpeed.slow):
+        elif (speed == FollowLineMotorSpeed.crossing):
             self.currentState = FollowLineState.reachCrossing
-            self._client.publish(self._roboName+"/request/steering/activate", "{\"speed\":\"3\",\"steering\":\"0\"}")
+            self._client.publish(self._roboName+"/request/steering/turn", "{\"speed\":\"30\",\"steering\":\"0\",\"angle\":\"250\"}")
             return
         elif (speed == FollowLineMotorSpeed.fast):
             self.currentState = FollowLineState.driving
