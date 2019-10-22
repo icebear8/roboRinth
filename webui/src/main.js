daa3a1b8992dd13936ce151f44d0a6a1ba79944d
reloZid <relozid@logixisland.ch>
2019-10-22 10:42:51 +0200

    cleanup web ui code

--- webui/src/main.js
+++ webui/src/main.js
@@ -5,7 +5,15 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
 import './main.css'
 
+let websocket = null;
+let network = null;
+
 $(document).ready(function() {
+    initNetwork();
+    initWebSocket();
+});
+
+function initNetwork() {
     const data = {
         nodes: [],
         edges: [],
@@ -18,48 +26,56 @@ $(document).ready(function() {
             dragNodes: false,
         }
     };
-    const container = $('#network');
-    const network = new vis.Network(container[0], data, options);
+    network = new vis.Network(($('#network'))[0], data, options);
+}
 
-    let websocket = new WebSocket('ws://localhost:8765');
-
+function initWebSocket() {
+    websocket = new WebSocket('ws://localhost:8765');
     websocket.onmessage = function(msg) {
         const updateData = JSON.parse(msg.data);
         console.log(updateData);
 
-        const nodeId = node => `${node.x}:${node.y}`;
-
-        let edges = [];
-        for (const edgeData of updateData.edges) {
-            const edge = {
-                from: nodeId(edgeData.node1),
-                to: nodeId(edgeData.node2),
-            };
-            edges.push(edge);
-        }
-
-        let nodes = [];
-        for (const edgeData of updateData.edges) {
-            for (const nodeData of [edgeData.node1, edgeData.node2]) {
-                const node = {
-                    id: nodeId(nodeData),
-                    label: nodeId(nodeData),
-                    x: nodeData.x * 70,
-                    y: nodeData.y * 70,
-                };
-
-                if (nodeData.x === updateData.position.x && nodeData.y === updateData.position.y) {
-                    node.color = 'red';
-                }
-
-                nodes.push(node);
-            }
-        }
-        nodes = _.uniqBy(nodes, node => node.id);
-
         network.setData({
-            nodes: nodes,
-            edges: edges,
+            nodes: getNodes(updateData),
+            edges: getEdges(updateData),
         });
     };
-});
+}
+
+function getNodes(updateData) {
+    let nodes = [];
+    for (const edgeData of updateData.edges) {
+        for (const nodeData of [edgeData.node1, edgeData.node2]) {
+            const node = {
+                id: getNodeId(nodeData),
+                label: getNodeId(nodeData),
+                x: nodeData.x * 70,
+                y: nodeData.y * 70,
+            };
+
+            if (nodeData.x === updateData.position.x && nodeData.y === updateData.position.y) {
+                node.color = 'red';
+            }
+
+            nodes.push(node);
+        }
+    }
+    return _.uniqBy(nodes, node => node.id);
+}
+
+function getEdges(updateData) {
+    let edges = [];
+    for (const edgeData of updateData.edges) {
+        const edge = {
+            from: getNodeId(edgeData.node1),
+            to: getNodeId(edgeData.node2),
+            color: 'black',
+        };
+        edges.push(edge);
+    }
+    return edges;
+}
+
+function getNodeId(nodeData) {
+    return `${nodeData.x}:${nodeData.y}`;
+}
